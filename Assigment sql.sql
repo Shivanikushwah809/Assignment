781a0b803496871e57ff22a5f64788e90e8a714c
Shivanikushwah809 <95361886+Shivanikushwah809@users.noreply.github.com>
2022-01-06 12:49:09 +0530

    Add files via upload

---
+++ Assigment sql.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE filmIndustry;
+SHOW DATABASES;
+USE filmIndustry;
+CREATE TABLE Star(movie VARCHAR(20),actor VARCHAR(20),actress VARCHAR(20),syear  INT, sDirector VARCHAR(20));
+SELECT * FROM star;
+INSERT INTO star VALUES('mujhse Shaadi Karogi','Varun Dhawan','jacqueline',2019,'David Dhawan');
